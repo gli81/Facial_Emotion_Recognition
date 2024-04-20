6b14c83526783b4ab0ad7b62f645fef5871ec0b3
LGZ <liguangzheng998@hotmail.com>
2024-04-19 20:05:51 -0400

    worked on evaluate

---
+++ 01_code/evaluate_.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from typing import Dict
+import torch
+import torchvision.models as models
+import torch.nn as nn
+import loaders
+
+## TODO load a base/finetuned model
+## TODO get corresponding validation loader
+## validate on that loader, record the predicted and the target value
+
+def evaluate_and_get_metrics(
+    model_: "str",
+    evaluate_on: "str",
+    device: "str"
+) -> "Dict":
+    ## load model based on parameter model_
+    if model_ not in ["baseline", "upper", "lower", "left", "right"]:
+        raise ValueError("Invalid model")
+    model = models.resnet50()
+    num_classes=7
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    pretrained = torch.load("./saved_model/" + model_ + ".pth")
+    model.load_state_dict(pretrained["state_dict"])
+    model.to(device)
+    model.eval()
+    ## get loader based on the designated type of image
+    loader = loaders.get_loader(
+        mask=evaluate_on,
+        train=False
+    )
+    ## evaluate on designated type of image
+    with torch.no_grad():
+        val_loss = 0
+        total_examples = 0
+        correct_examples = 0
+        for batch_idx, (inputs, targets) in enumerate(lower_mask_val_loader):
+            # copy inputs to device
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            # compute the output and loss
+            out = resnet50(inputs)
+            loss = loss_func(out, targets)
+            # count the number of correctly predicted samples
+            # in the current batch
+            _, predicted = torch.max(out, 1)
+            correct = predicted.eq(targets).sum()
+            val_loss += loss.detach().cpu()
+            total_examples += targets.shape[0]
+            correct_examples += correct.item()
+    avg_loss = val_loss / len(lower_mask_val_loader)
+    avg_acc = correct_examples / total_examples
+    print(
+        "Validation loss: %.4f, Validation accuracy: %.4f" % (avg_loss, avg_acc)
+    )
