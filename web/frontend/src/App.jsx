2af28b311a8d17831584cb279911975cdfddc3c6
GLi81 <liguangzheng998@hotmail.com>
2024-09-24 10:37:39 -0400

    added documentation for frontend

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -1,11 +1,18 @@
+/**
+ * Frontend code to handle image upload and model selection.
+ */
+
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   // image preview state
   const [imagePreview, setImagePreview] = useState(null);
+  // radio button change
+  const [model, setModel] = useState("full");
+  const handleChange = (e) => {
+    setModel(e.target.value);
+  }
   // image upload event
   const handleImageUpload = (e) => {
     // get the image file
@@ -36,7 +43,7 @@ function App() {
       // const base64img = await resizeAndConvertToBase64(imagePreview)
       const ops = {
         image: imagePreview,
-        model: "full"
+        model: model
       }
 
     // send the image to the server
@@ -68,6 +75,24 @@ function App() {
             style={{ width: '300px', height: '300px', objectFit: 'cover' }}
           />
         )}
+        <br />
+        {/* radio button to select model , choices are: full, left, right, upper, lower*/}
+        <br />
+        <input type="radio" id="full" name="model"
+                value="full" checked={model === 'full'}
+                onChange={handleChange} />FULL<br />
+        <input type="radio" id="left" name="model"
+                value="left" checked={model === 'left'}
+                onChange={handleChange} />LEFT<br />
+        <input type="radio" id="right" name="model"
+                value="right" checked={model === 'right'}
+                onChange={handleChange} />RIGHT<br />
+        <input type="radio" id="upper" name="model"
+                value="upper" checked={model === 'upper'}
+                onChange={handleChange} />UPPER<br />
+        <input type="radio" id="lower" name="model"
+                value="lower" checked={model === 'lower'}
+                onChange={handleChange} />LOWER<br />
         <button onClick={handleImageSubmit}>Submit</button>
       </header>
     </div>
